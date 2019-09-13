9f54d086ababd72e0ef5bc21203f97a7db0d7965
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-09-13 19:06:12 +0700

    'A'

--- app/src/main/java/com/example/progressbar29072019/MainActivity.java
+++ app/src/main/java/com/example/progressbar29072019/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.progressbar29072019;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    //    SeekBar sk;
+//    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        progressBar.findViewById(R.id.progressbar);
+//        progressBar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//        sk = findViewById(R.id.seekbar);
+//        sk.setMax(10000);
+//        sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+//                Log.d("BBB", i + "");
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });
     }
 }
